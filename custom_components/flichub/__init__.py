7b2f9d80d7e47a556bf2be619e9c2abb4301fea3
Johan Nenzén <johan.nanzen@gmail.com>
2023-02-28 18:00:29 +0100

    Setup using config flow

--- custom_components/flichub/__init__.py
+++ custom_components/flichub/__init__.py
@@ -32,12 +32,6 @@ SCAN_INTERVAL = timedelta(seconds=30)
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
-
-async def async_setup(hass: HomeAssistant, config: Config):
-    """Set up this integration using YAML is not supported."""
-    return True
-
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up this integration using UI."""
     if hass.data.get(DOMAIN) is None:
